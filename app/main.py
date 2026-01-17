56767ba90abc5f77f922494520f15bbd63a06504
Andriy <andrijbuluk@gmail.com>
2026-01-17 14:33:41 +0100

    Сlean up file Fix flake8

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import os
 
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
 
